59a084253fc849b85c84683ea7842e6221238a08
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-10-01 17:43:21 -0600

    feat: a lot of shareable scripts

---
+++ mullman/default.nix
@@ -0,0 +1,16 @@
+{pkgs ? import <nixpkgs> {}}:
+with pkgs;
+  ocamlPackages.buildDunePackage rec {
+    pname = "mullman";
+    version = "unstable";
+
+    src = builtins.path {
+      name = pname;
+      path = ./.;
+    };
+
+    # checkInputs = [alcotest ppx_let];
+    # buildInputs = [ocaml-syntax-shims];
+    propagatedBuildInputs = [ocamlPackages.result];
+    doCheck = lib.versionAtLeast ocaml.version "5.0.0";
+  }
