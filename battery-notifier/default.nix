be6e4f442e741f3ca69cbfc7a0b2a755f4ba5084
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-11-17 14:29:45 -0500

    fix(battery-notifier): libnotify as runtime package

--- battery-notifier/default.nix
+++ battery-notifier/default.nix
@@ -1,35 +1,43 @@
-{pkgs ? import <nixpkgs> {}, ...}:
-pkgs.rustPlatform.buildRustPackage rec {
-  pname = "battery-notifier";
-  version = "unstable";
+{pkgs ? import <nixpkgs> {}, ...}: let
+  runtimePackages = with pkgs; [libnotify];
+in
+  pkgs.rustPlatform.buildRustPackage rec {
+    pname = "battery-notifier";
+    version = "unstable";
 
-  src = builtins.path {
-    name = pname;
-    path = ./.;
-  };
-
-  nativeBuildInputs = with pkgs; [git cmake];
-
-  # buildInputs = with pkgs; [miniaudio];
-
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-
-    outputHashes = {
-      "soloud-1.0.5" = "sha256-2Cd5aWfntRawxRSdy+4tJJdTkTeii1ilshQadG6Pybw=";
+    src = builtins.path {
+      name = pname;
+      path = ./.;
     };
-  };
 
-  preConfigure = ''
-    substituteInPlace ./src/main.rs \
-    --replace './../assets' $src/assets
+    nativeBuildInputs = with pkgs; [git cmake makeWrapper];
+    buildInputs = runtimePackages;
 
-    substituteInPlace ./src/main.rs \
-      --replace './assets' '${placeholder "out"}/assets'
-  '';
+    # buildInputs = with pkgs; [miniaudio];
 
-  postInstall = ''
-    mkdir -p $out/assets/
-    cp ./assets/*.png $out/assets/
-  '';
-}
+    cargoLock = {
+      lockFile = ./Cargo.lock;
+
+      outputHashes = {
+        "soloud-1.0.5" = "sha256-2Cd5aWfntRawxRSdy+4tJJdTkTeii1ilshQadG6Pybw=";
+      };
+    };
+
+    preConfigure = ''
+      substituteInPlace ./src/main.rs \
+      --replace './../assets' $src/assets
+
+      substituteInPlace ./src/main.rs \
+        --replace './assets' '${placeholder "out"}/assets'
+    '';
+
+    preInstall = ''
+      mkdir -p $out/assets/
+      cp ./assets/*.png $out/assets/
+    '';
+
+    postInstall = ''
+      wrapProgram ${placeholder "out"}/bin/${pname} \
+        --prefix PATH : ${pkgs.lib.makeBinPath runtimePackages}
+    '';
+  }
