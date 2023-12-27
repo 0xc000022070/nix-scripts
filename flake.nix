ad0fadc0f963dec6a41bd9b9a42c0503967faed8
Luis Quiñones <lpaandres2020@gmail.com>
2023-12-27 13:51:46 -0500

    feat(nix): home manager module for rofi radio

--- flake.nix
+++ flake.nix
@@ -5,41 +5,47 @@
     systems.url = "github:nix-systems/default-linux";
   };
 
-  outputs = inputs:
-    with inputs; let
-      inherit (nixpkgs) lib;
+  outputs = inputs @ {
+    self,
+    poetry2nix,
+    systems,
+    nixpkgs,
+  }: let
+    inherit (nixpkgs) lib;
 
-      pkgsFor = eachSystem (system:
-        import nixpkgs {
-          localSystem = system;
-        });
+    pkgsFor = eachSystem (system:
+      import nixpkgs {
+        localSystem = system;
+      });
 
-      eachSystem = lib.genAttrs (import systems);
-    in {
-      packages = eachSystem (system: let
-        pkgs = pkgsFor.${system};
+    eachSystem = lib.genAttrs (import systems);
+  in {
+    packages = eachSystem (system: let
+      pkgs = pkgsFor.${system};
 
-        entries = {
-          dunstify-brightness = ./dunstify-brightness;
-          battery-notifier = ./battery-notifier;
-          playerctl-waybar = ./playerctl-waybar;
-          dunstify-sound = ./dunstify-sound;
-          screen-capture = ./screen-capture;
-          cliphist-rofi = ./cliphist-rofi;
-          swww-switcher = ./swww-switcher;
-          spotify-dbus = ./spotify-dbus;
-          rofi-radio = ./rofi-radio;
-          mullx = ./mullx;
-        };
-      in
-        {
-          default = self.packages.${system}.rofi-radio;
-        }
-        // lib.attrsets.mapAttrs (_n: p:
-          pkgs.callPackage p {
-            inherit (poetry2nix.lib.mkPoetry2Nix {inherit pkgs;}) mkPoetryApplication;
-            inherit pkgs;
-          })
-        entries);
-    };
+      entries = {
+        dunstify-brightness = ./dunstify-brightness;
+        battery-notifier = ./battery-notifier;
+        playerctl-waybar = ./playerctl-waybar;
+        dunstify-sound = ./dunstify-sound;
+        screen-capture = ./screen-capture;
+        cliphist-rofi = ./cliphist-rofi;
+        swww-switcher = ./swww-switcher;
+        spotify-dbus = ./spotify-dbus;
+        rofi-radio = ./rofi-radio;
+        mullx = ./mullx;
+      };
+    in
+      {
+        default = self.packages.${system}.rofi-radio;
+      }
+      // lib.attrsets.mapAttrs (_n: p:
+        pkgs.callPackage p {
+          inherit (poetry2nix.lib.mkPoetry2Nix {inherit pkgs;}) mkPoetryApplication;
+          inherit pkgs;
+        })
+      entries);
+
+    homeManagerModules.default = import ./nix/hm-module.nix self;
+  };
 }
