b3d1f171f68110534a2e660026666b958663d123
Luis Quiñones <lpaandres2020@gmail.com>
2023-12-31 00:29:20 -0500

    feat(pstore): usage + help flag + exit codes

--- pstore/bin/main.ml
+++ pstore/bin/main.ml
@@ -13,10 +13,23 @@ let search_in_nix_store search_term =
   in
   List.iter (fun x -> Printf.printf "%s\n" x) filtered_entries
 
+let print_usage exit_code =
+  Printf.printf
+    "Get /nix/store paths that match the given argument.\n\n\
+     Usage:\n\
+    \ pstore <search-term>\n\n\
+     Flags:\n\
+    \  -h, --help\tHelp for this program\n";
+  exit exit_code
+
 let main () =
-  let term =
-    if Array.length Sys.argv == 1 then "<><><><><><><><>" else Sys.argv.(1)
+  let code =
+    if Array.length Sys.argv == 1 then 1
+    else if List.mem Sys.argv.(1) [ "--help"; "-h" ] then 0
+    else -1
   in
-  search_in_nix_store term
+  match code with
+  | -1 -> search_in_nix_store Sys.argv.(1)
+  | _ -> print_usage code
 
 let () = main ()
