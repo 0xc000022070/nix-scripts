41fa4a84508b6d5bb8db9e92a5fb611d8fc073e9
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-26 14:13:07 -0500

    nix(battery-notifier): defined settings for home manager

--- nix/hm-module.nix
+++ nix/hm-module.nix
@@ -8,12 +8,37 @@ self: {
 in
   with lib; let
     inherit (pkgs.stdenv.hostPlatform) system;
+    tomlFormat = pkgs.formats.toml {};
 
     flake-pkgs = self.packages.${system};
   in {
     options.programs = {
       battery-notifier = {
         enable = mkEnableOption "battery-notifier";
+
+        settings = types.submodule {
+          options = {
+            sleep_ms = mkOption {
+              type = types.int;
+              default = 700;
+            };
+
+            reminder_threshold = mkOption {
+              type = types.int;
+              default = 30;
+            };
+
+            warn_threshold = mkOption {
+              type = types.int;
+              default = 15;
+            };
+
+            threat_threshold = mkOption {
+              type = types.int;
+              default = 5;
+            };
+          };
+        };
       };
 
       rofi-radio = let
@@ -51,25 +76,51 @@ in
         then v
         else {};
     in
-      ifOrEmptySet hmConfig.programs.battery-notifier.enable {
-        systemd.user.services = {
-          battery-notifier = {
-            Unit = {
-              Description = "A very useful battery notifier for window managers";
-            };
+      (
+        let
+          cfg = hmConfig.programs.battery-notifier;
+        in
+          ifOrEmptySet cfg.enable {
+            assertions = [
+              {
+                assertion = builtins.length (lib.attrsets.attrValues (builtins.filterAttrs (k: v: lib.strings.hasSuffix k "threshold" && v >= 0 && v <= 100) cfg.settings)) == 0;
+                message = "threshold values must be greater equal than 0 and less equal than 100";
+              }
+              {
+                assertion = cfg.settings.reminder_threshold > cfg.settings.warn_threshold;
+                message = "'reminder' threshold must be greater than 'warn' threshold";
+              }
+              {
+                assertion = cfg.settings.warn_threshold > cfg.settings.threat_threshold;
+                message = "'warn' threshold must be greater than 'threat' threshold";
+              }
+              {
+                assertion = cfg.settings.sleep_ms > 0;
+                message = "sleep time must be greater than zero";
+              }
+            ];
 
-            Service = {
-              Type = "simple";
-              ExecStart = "${flake-pkgs.battery-notifier}/bin/battery-notifier";
-              Restart = "on-failure";
-            };
+            xdg.configFile."battery-notifier/config.toml".source = tomlFormat.generate "battery-notifier-config" cfg.settings;
 
-            Install = {
-              WantedBy = ["default.target"];
+            systemd.user.services = {
+              battery-notifier = {
+                Unit = {
+                  Description = "A very useful battery notifier for window managers";
+                };
+
+                Service = {
+                  Type = "simple";
+                  ExecStart = "${flake-pkgs.battery-notifier}/bin/battery-notifier";
+                  Restart = "on-failure";
+                };
+
+                Install = {
+                  WantedBy = ["default.target"];
+                };
+              };
             };
-          };
-        };
-      }
+          }
+      )
       // (
         let
           cfg = hmConfig.programs.rofi-radio;
